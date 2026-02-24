27628f9e14960ce47d7f7dd0fad1bccfa53de26d
lasse <77295879+lassejlv@users.noreply.github.com>
2026-02-24 02:17:08 +0100

    Update Termy to version 0.1.29

--- Casks/termy.rb
+++ Casks/termy.rb
@@ -1,9 +1,9 @@
 cask "termy" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.28"
-  sha256 arm:   "e573efd74b355590745478fdd2d63a580070d53383f7b93b93ba4764a6ccc74f",
-         intel: "9c7d05a0c81187af4d9c626b41d336cd6a5b0995710c98741f161226b5bc4402"
+  version "0.1.29"
+  sha256 arm:   "ceda4d7bb220221453786ffab9dcf1d20b689b18183fa6b105d238e1d639001c",
+         intel: "fe011a85c80c6b8ec821eb4de908e1fcec2844779ed7566f292bbcb48e3746fe"
 
   url "https://github.com/lassejlv/termy/releases/download/v#{version}/Termy-v#{version}-macos-#{arch}.dmg"
   name "Termy"
